f02628d1707a5b74f583df70671d5c7e423b7e50
Emmanuel Otoo <eotoo218@gmail.com>
2026-02-19 12:17:45 +0000

    refactor: move BackButton component to a new file and update import path

--- src/app/(routes)/layout.tsx
+++ src/app/(routes)/layout.tsx
@@ -1,4 +1,4 @@
-import { BackButton } from "@/components/back-btn";
+import { BackButton } from "./components/back-button";
 import { ReactNode } from "react";
 
 export default function Layout({ children }: { children: ReactNode }) {
