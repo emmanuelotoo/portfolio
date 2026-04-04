92ceb6eb6822d94f701dbe62fc9f5e617415336c
Emmanuel Otoo <eotoo218@gmail.com>
2026-04-04 16:21:19 +0000

    feat: add Comments component to enable user interaction on the page

---
+++ src/components/comments.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import Giscus from "@giscus/react";
+
+export default function Comments() {
+  return (
+    <section className="mt-12 border-t border-secondary-foreground/10 pt-8">
+      <Giscus
+        repo="emmanuelotoo/portfolio"
+        repoId="R_kgDOPZCvlA"
+        category="General"
+        categoryId="DIC_kwDOPZCvlM4C6C8f"
+        mapping="pathname"
+        reactionsEnabled="1"
+        emitMetadata="0"
+        inputPosition="top"
+        theme="dark_tritanopia"
+        lang="en"
+        loading="lazy"
+      />
+    </section>
+  );
+}
