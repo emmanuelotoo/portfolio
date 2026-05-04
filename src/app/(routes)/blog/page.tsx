9738b66476748fb39c7711ecaac45cece815481b
Emmanuel Otoo <eotoo218@gmail.com>
2026-05-04 21:59:57 +0000

    feat: remove Blog component to simplify blog routing

--- src/app/(routes)/blog/page.tsx
+++
@@ -1,7 +0,0 @@
-import { getAllPosts } from "@/lib/blog-data";
-import BlogList from "./blog-list";
-
-export default function Blog() {
-  const posts = getAllPosts();
-  return <BlogList posts={posts} />;
-}
